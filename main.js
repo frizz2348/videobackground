43046136ab36350852033a9e337dc8d6f92f8cee
Frizzle K <frizz2348@gmail.com>
2021-04-29 20:43:30 +0800

    null

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ menuToggle.addEventListener('click',  () => {
     menuToggle.classList.toggle('active')
     showcase.classList.toggle('active')
 });
+
+showcase.addEventListener('click', function() {
+    menuToggle.classList.toggle('active')
+    showcase.classList.toggle('active')
+});
